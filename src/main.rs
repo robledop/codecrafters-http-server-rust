4a09788b432c692e8f1d13b82b3f43c41131f6c0
Robledo Pazotto <robledo@gmail.com>
2025-03-12 13:10:17 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[allow(unused_imports)]
 use std::env;
 use std::fs;
-use std::io::{BufRead, BufReader, Write};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -19,7 +20,7 @@ fn main() {
 
         i += 1;
     }
-    
+
     if directory != "" {
         println!("Using directory: {}", directory);
     }
@@ -29,7 +30,7 @@ fn main() {
     for stream in listener.incoming() {
         if let Ok(stream) = stream {
             println!("accepted new connection");
-            
+
             let dir = directory.clone();
 
             std::thread::spawn(|| handle_request(stream, dir));
@@ -40,59 +41,100 @@ fn main() {
 }
 
 fn handle_request(mut stream: TcpStream, directory: String) {
-    let buffer_reader = BufReader::new(&mut stream);
-    let mut buffer_lines = buffer_reader.lines();
-    if let Some(Ok(first_line)) = buffer_lines.next() {
-        let parts: Vec<&str> = first_line.split(' ').collect();
-        let (method, path) = (parts[0], parts[1]);
+    let mut buffer_reader = BufReader::new(&mut stream);
+    let mut request_line = String::new();
+    buffer_reader.read_line(&mut request_line).unwrap();
+    let parts: Vec<&str> = request_line.split(' ').collect();
+    let (method, path) = (parts[0], parts[1]);
 
-        let response = match method {
-            "GET" => match path {
-                "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
-                p if p.starts_with("/echo/") => {
-                    let message = &path[6..];
-                    format!(
-                        "HTTP/1.1 200 OK\r\n\
+    let mut headers = Vec::new();
+
+    loop {
+        let mut line = String::new();
+        buffer_reader.read_line(&mut line).unwrap();
+        let line = line.trim_end();
+        if line.is_empty() {
+            break; // If the line is empty, it means this is the end of the headers
+        }
+
+        headers.push(line.to_string());
+    }
+
+    let response = match method {
+        "GET" => match path {
+            "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+            p if p.starts_with("/echo/") => {
+                let message = &path[6..].trim().to_string();
+                format!(
+                    "HTTP/1.1 200 OK\r\n\
                                 Content-Type: text/plain\r\n\
                                 Content-Length: {}\r\n\r\n{}",
-                        message.len(),
-                        message
-                    )
-                }
-                "/user-agent" => {
-                    let user_agent_line = buffer_lines
-                        .find(|x| x.as_ref().unwrap().starts_with("User-Agent: "))
-                        .unwrap()
-                        .expect("Invalid user-agent line");
-                    let user_agent = &user_agent_line[12..];
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent)
-                }
-                p if p.starts_with("/files/") => {
-                    let file_name = &path[7..];
-                    let file_path = format!("{}/{}", directory, file_name);
+                    message.len(),
+                    message
+                )
+            }
+            "/user-agent" => {
+                let user_agent_line = headers
+                    .iter()
+                    .find(|&x| x.starts_with("User-Agent: "))
+                    .unwrap();
+                let user_agent = &user_agent_line[12..].trim().to_string();
+                format!(
+                    "HTTP/1.1 200 OK\r\n\
+                    Content-Type: text/plain\r\n\
+                    Content-Length: {}\
+                    \r\n\r\n\
+                    {}",
+                    user_agent.len(),
+                    user_agent
+                )
+            }
+            p if p.starts_with("/files/") => {
+                let file_name = &path[7..].trim().to_string();
+                let file_path = format!("{}{}", directory, file_name);
 
-                    if fs::exists(&file_path).unwrap() {
-                        let file_content = fs::read_to_string(&file_path).unwrap();
+                let file_content = fs::read_to_string(&file_path);
 
-                        format!(
-                            "HTTP/1.1 200 OK\r\n\
-                    Content-Type: application/octet-stream\r\n\
-                    Content-Length: {}\r\n\r\n{}",
-                            file_content.len(),
-                            file_content
-                        )
-                    } else {
-                        "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
-                    }
+                match file_content {
+                    Ok(file) => format!(
+                        "HTTP/1.1 200 OK\r\n\
+                        Content-Type: application/octet-stream\r\n\
+                        Content-Length: {}\r\n\r\n{}",
+                        file.len(),
+                        file
+                    ),
+                    Err(_) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
                 }
-                _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
-            },
-            "POST" => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
-            _ => "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_string(),
-        };
+            }
+            _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+        },
+        "POST" => match path {
+            p if p.starts_with("/files/") => {
+                let file_name = &path[7..].trim().to_string();
+                let file_path = format!("{}{}", directory, file_name);
 
-        stream
-            .write_all(response.as_bytes())
-            .expect("Failed to write to server");
-    }
+                let content_length: usize = headers
+                    .iter()
+                    .find(|&x| x.starts_with("Content-Length: "))
+                    .unwrap()[16..]
+                    .trim()
+                    .parse()
+                    .unwrap();
+
+                let mut body_buffer = vec![0; content_length];
+                buffer_reader.read_exact(&mut body_buffer).unwrap();
+
+                let mut file = File::create(&file_path).unwrap();
+                file.write_all(&body_buffer).unwrap();
+
+                "HTTP/1.1 201 Created\r\n\r\n".to_string()
+            }
+            _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+        },
+        _ => "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_string(),
+    };
+
+    stream
+        .write_all(response.as_bytes())
+        .expect("Failed to write to server");
 }
