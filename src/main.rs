e3018abe2705ca3d11b9426a5c8c2d55dfa29e7e
Robledo Pazotto <robledo@gmail.com>
2025-03-12 08:06:10 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
+use std::io::{BufRead, Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::sync::Mutex;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
-     for stream in listener.incoming() {
-         match stream {
-             Ok(_stream) => {
-                 println!("accepted new connection");
-             }
-             Err(e) => {
-                 println!("error: {}", e);
-             }
-         }
-     }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+
+                // let mut read_buffer = String::new();
+                // stream.read_to_string(&mut read_buffer).expect("read failed");
+                // 
+                // println!("RECEIVED: {}", read_buffer);
+                // 
+                // let lines: Vec<&str> = read_buffer.split("\r\n").collect();
+                // 
+                // let first_line = lines[0].split(" ").collect::<Vec<&str>>();
+                // let (method, path) = (first_line[0], first_line[1]);
+                // 
+                // println!("method: {}, path: {}", method, path);
+
+                stream
+                    .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .expect("Failed to write to server");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
