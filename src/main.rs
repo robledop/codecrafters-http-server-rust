6cd57ab5c8b863213f24bcb2ec043ab508e3e6c2
Robledo Pazotto <robledo@gmail.com>
2025-03-12 08:38:10 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,44 +6,27 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
+        if let Ok(mut stream) = stream {
+            println!("accepted new connection");
 
-                let buffer_reader = BufReader::new(&mut stream);
-                let first_line = buffer_reader
-                    .lines()
-                    .next()
-                    .unwrap()
-                    .expect("failed to read first line");
+            let buffer_reader = BufReader::new(&mut stream);
+            if let Some(Ok(first_line)) = buffer_reader.lines().next() {
+                let parts: Vec<&str> = first_line.split(' ').collect();
+                let (method, path) = (parts[0], parts[1]);
 
-                let first_line_split = first_line.split(" ").collect::<Vec<&str>>();
-                let (method, path) = (first_line_split[0], first_line_split[1]);
+                let response = match method {
+                    "GET" => match path {
+                        "/" => "HTTP/1.1 200 OK\r\n\r\n",
+                        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                    },
+                    "POST" => "HTTP/1.1 404 Not Found\r\n\r\n",
+                    _ => "HTTP/1.1 405 Method Not Allowed\r\n\r\n",
+                };
 
-                let response;
-
-                match method {
-                    "GET" => {
-                        response = match path {
-                            "/" => "HTTP/1.1 200 OK\r\n\r\n",
-                            _ => "HTTP/1.1 404 Not Found\r\n\r\n",
-                        };
-                    }
-                    "POST" => {
-                        response = "HTTP/1.1 200 OK\r\n\r\n";
-                    }
-                    _ => {
-                        response = "HTTP/1.1 404 MethodNotAllowed\r\n\r\n";
-                    }
-                }
-
-                stream
-                    .write(response.as_bytes())
-                    .expect("Failed to write to server");
+                stream.write_all(response.as_bytes()).expect("Failed to write to server");
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        } else {
+            eprintln!("error: {}", stream.unwrap_err());
         }
     }
 }
